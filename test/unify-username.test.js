6aad7f49e1ec533c21bf3ef2b5f5d86233070360
Pavel lysenko <p.lysenko@zmeke.com>
2016-11-10 14:02:34 +0300

    Add test

---
+++ test/unify-username.test.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const chai          = require('chai'),
+      assert        = chai.assert,
+      unifyUsername = require('./../scripts/unify-username');
+
+describe(
+  'unifyUsername', () => {
+    it(
+      'Возвращает функцию', () => {
+        assert.isFunction(unifyUsername);
+      }
+    );
+
+    describe(
+      'Правильно работает при корректных данных', () => {
+
+        it(
+          'https://vk.com/skillbranch', () => {
+            assert.equal(unifyUsername('https://vk.com/skillbranch'), '@skillbranch');
+          }
+        );
+
+        it(
+          '//vk.com/skillbranch', () => {
+            assert.equal(unifyUsername('//vk.com/skillbranch'), '@skillbranch');
+          }
+        );
+
+        it(
+          'skillbranch', () => {
+            assert.equal(unifyUsername('skillbranch'), '@skillbranch');
+          }
+        );
+
+        it(
+          'https://vk.com/skillbranch?w=wall-117903599_1076', () => {
+            assert.equal(unifyUsername('https://vk.com/skillbranch?w=wall-117903599_1076'), '@skillbranch');
+          }
+        );
+
+      }
+    );
+
+    describe(
+      'Возвращает ошибку при некорректных данных', () => {
+
+        it(
+          '\'\'', () => {
+            assert.throws(() => {unifyUsername('')}, 'Invalid username');
+          }
+        );
+
+        it(
+          '\?', () => {
+            assert.throws(() => {unifyUsername('\?')}, 'Invalid username');
+          }
+        );
+
+      }
+    );
+  }
+);
